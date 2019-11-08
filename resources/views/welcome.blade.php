e8b1ace2bb16a2a2b75c20d88c735350220baf6a
mweber <mweber@kzvnr.de>
2019-11-08 14:43:12 +0100

    scaffolded Login / Register UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BugSpring
                 </div>
 
+
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://bugspring.com/">Homepage</a>
+                    <a href="https://github.com/bugspring/BugSpring">GitHub</a>
                 </div>
             </div>
         </div>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['namespace' => 'Api'], function()
+{
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
+
+    Route::group(['middleware' => 'auth:api'], function()
+    {
+        Route::post('/users/details', 'UserController@details');
+    });
+
 });
+
